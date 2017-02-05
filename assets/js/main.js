1aa36a70252a3852cef5c64b8ca5c684d19ff478
Gunnar Normand <gunnarnormand@yahoo.com>
2017-02-05 16:47:14 -0500

    mvp done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,23 @@ $(document).ready(function(){
 		// findShapeIndex("#start", "#end");
 	
 		var controller = new ScrollMagic.Controller();
+
+		var panel1Timeline = new TimelineMax()
+			.from(".panel-content-one h2", 1, {opacity: 0, ease: Power0.easeNone})
+			.from(".panel-content-one h2", 1, {x: 250, ease: Power0.easeNone}, "-=1")
+			.from(".panel-content-one p.left", 1.5, {x: 250, ease: Power0.easeNone}, "-=1")
+			.from(".panel-content-one p.left", 1.5, {opacity: 0, ease: Power0.easeNone}, "-=1")
+
+			.from(".panel-content-one h6", 1, {y: '-100%', ease: Power0.easeNone}, "-=.25")
+			.from(".panel-content-one h1", 1, {y: '-100%', ease: Power0.easeNone}, "-=.25")
+			.from(".panel-content-one p.right", 1, {y: '-100%', ease: Power0.easeNone}, "-=.25");
+
+			// .from(".album-wrap", .5, {y: '-100%', ease: Power0.easeNone}, "-=.5")
+			// .from(".album-wrap", .5, {y: '-100%', ease: Power0.easeNone}, "-=.5")
+			// .from(".album-wrap", .5, {y: '-100%', ease: Power0.easeNone}, "-=.5");
+
 	
-		var wipeAnimation = new TimelineMax()
+		var masterTimeline = new TimelineMax()
 		 	.to("#start", 4, {morphSVG:{shape:"#end", shapeIndex:16}, ease: Elastic.easeOut.config(1, 0.3)}, "frame1")
 		 	.to("#start", 4, {fill: '#fff', ease: Circ.easeOut}, "frame1")
 		 	.to("#start", 4, {opacity: 0, ease: Circ.easeOut}, "frame1+=3")
@@ -16,26 +31,26 @@ $(document).ready(function(){
 			.fromTo("#panel--one-second", 5, {x: -3000}, {x: 3000, ease: CustomEase.create("custom", "M0,0,C0,0,0.509,0.293,0.524,0.298,0.776,0.383,0.586,0.52,0.875,0.612,0.891,0.617,0.904,0.623,0.915,0.634,0.928,0.648,0.936,0.664,0.945,0.683,0.955,0.707,0.96,0.725,0.965,0.751,0.981,0.846,1,1,1,1")}, "frame1+=0.9")
 			.fromTo("#panel--one-third", 10, {x: 2000}, {x: -3000, ease: Power0.easeNone}, "frame1")
 			
-			// animate to second panel
-			// .to("#slide-wrap", .25, {z: -150, ease: Power0.easeNone})		// move back in 3D space
-			// .to("#slide-wrap", .25, {z: 0, ease: Power0.easeNone})				// move back to origin in 3D space
-			
+			// add nested panelTimeline
+			.add(panel1Timeline, "frame1+=2")
+
 			.fromTo("#panel--two", 10, {x: window.innerWidth}, {x: -3000, ease: Power0.easeNone}, "frame2")
 			.to("#start1", 8, {morphSVG:{shape:"#end1", shapeIndex:16}, ease: Circ.easeOut}, "frame2-=5.5")
 			
-			// animate to third panel
-			// .to("#slide-wrap", .25, {z: -150, ease: Power0.easeNone})
-			// .to("#slide-wrap", .25, {z: 0, ease: Power0.easeNone})
-			
+			// add nested panelTimeline
+
 			.fromTo("#panel--three", 10, {x:  window.innerWidth}, {x: -3000, ease: Power0.easeNone})
 			
-			// animate to fourth panel
-			// .to("#slide-wrap", .25, {z: -150, ease: Power0.easeNone})
-			// .to("#slide-wrap", .25, {z: 0, ease: Power0.easeNone})
-			
-			.fromTo("#panel--four", 10, {x: window.innerWidth}, {x: -3000, ease: Power0.easeNone});
+			// add nested panelTimeline
 
-		
+			
+			.fromTo("#panel--four", 10, {x: window.innerWidth}, {x: -3000, ease: Power0.easeNone})
+
+			// add nested panelTimeline
+
+			.fromTo("#panel--five", 5, {y: window.innerHeight}, {y: '100vh', ease: Power0.easeNone}, "-=2.5");
+
+			// add nested panelTimeline
 
 
 
@@ -52,7 +67,7 @@ $(document).ready(function(){
 				duration: "1000%"
 			})
 			.setPin("#main-wrap")
-			.setTween(wipeAnimation)
+			.setTween(masterTimeline)
 			.addIndicators()
 			.addTo(controller);
 });
--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -19,11 +19,10 @@ $(document).ready(function(){
 				   	.to("#first", .2, {morphSVG:"#first", shapeIndex:-2, ease: Circ.easeOut}, "+=1");
 
 
-	$('#top').on('click', function() {
+	$('.logo-2').on('click', function() {
 
-		TweenMax.to("#start", 1, {morphSVG:{shape:"#end", shapeIndex:28}, ease: Elastic.easeOut.config(1, 0.3)});
-		TweenMax.to("#end", 1, {opacity: 0, ease:Bounce.easeOut});
-
+		TweenMax.to("#start-2", 1, {morphSVG:{shape:"#end-2", shapeIndex:28}, ease: Elastic.easeOut.config(1, 0.3)});
+		TweenMax.to("#start-2", 1, {fill: '#fff', ease: Circ.easeOut});
 	});
 	
 	$('#backtop').on('click', function() {
