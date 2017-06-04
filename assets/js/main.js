270670fbbcee9bd9cd1916c69d3ecb937298b239
Gunnar Normand <gunnarnormand@yahoo.com>
2017-06-04 02:35:12 -0400

    switched to gsap minified

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 		var controller = new ScrollMagic.Controller();
 
 		var panel1Timeline = new TimelineMax()
+			.to(".nav-1-img", .5, {className:"+=nav-img-active"})
 			.from(".panel-content-one h2", 1, {opacity: 0, ease: Power0.easeNone})
 			.from(".panel-content-one h2", 1, {x: 250, ease: Power0.easeNone}, "-=1")
 			.from(".panel-content-one p.left", 1.5, {x: 250, ease: Power0.easeNone}, "-=1")
@@ -22,11 +23,7 @@ $(document).ready(function(){
 			.from(".panel-content-one p.right", .75, {y: '-100%', ease: Power0.easeNone}, "-=.25");
 
 			var panel5Timeline = new TimelineMax()
-				.staggerFrom("footer h1", 1, {y: window.innerHeight, ease: Back.easeOut.config(1.7)})
-				.staggerFrom("footer .p1", 1, {y: window.innerHeight, ease: Back.easeOut.config(1.7)}, "-=1")
-				.staggerFrom("footer form.f1", 1, {y: window.innerHeight, ease: Back.easeOut.config(1.7)}, "-=1")
-				.staggerFrom("footer form.f2", 1, {y: window.innerHeight, ease: Back.easeOut.config(1.7)}, "-=1")
-				.staggerFrom("footer .p2", 1, {y: window.innerHeight, ease: Back.easeOut.config(1.7)}, "-=1");
+				.from("footer .logo2", 1, {rotation: 360, ease: Power0.easeNone});
 	
 		var masterTimeline = new TimelineMax()
 			
@@ -36,34 +33,34 @@ $(document).ready(function(){
 		 	// main logo tween
 		 	.to("#start", 4, {morphSVG:{shape:"#end", shapeIndex:16}, ease: Elastic.easeOut.config(1, 0.3)}, "frame1")
 		 	.to("#start", 4, {fill: '#fff', ease: Circ.easeOut}, "frame1")
-		 	.to("#start", 4, {opacity: 0, ease: Circ.easeOut}, "frame1+=3")
+		 	.to("#start", 2, {opacity: 0, ease: Circ.easeOut}, "frame1+=2")
 			
 			// first panels tweens
-			.fromTo("#panel--one", 10, {x: 2000}, {x: -3000, ease: Power0.easeNone}, "frame1")
-			.fromTo("#panel--one-second", 5, {x: -3000}, {x: 3000, ease: CustomEase.create("custom", "M0,0,C0,0,0.509,0.293,0.524,0.298,0.776,0.383,0.586,0.52,0.875,0.612,0.891,0.617,0.904,0.623,0.915,0.634,0.928,0.648,0.936,0.664,0.945,0.683,0.955,0.707,0.96,0.725,0.965,0.751,0.981,0.846,1,1,1,1")}, "frame1+=0.9")
-			.fromTo("#panel--one-third", 10, {x: 2000}, {x: -3000, ease: Power0.easeNone}, "frame1")
+			.fromTo("#panel--one", 10, {x: 2000, force3D:true}, {x: -3000, force3D:true, ease: Power0.easeNone}, "frame1")
+			.fromTo("#panel--one-second", 5, {x: -3000, force3D:true}, {x: 3000, force3D:true, ease: Power0.easeNone}, "frame1+=0.9")
+			.fromTo("#panel--one-third", 10, {x: 2000, force3D:true}, {x: -3000, force3D:true, ease: Power0.easeNone}, "frame1")
 			
 
 			// add nested panelTimeline
 			.add(panel1Timeline, "frame1+=2")
 
 			// second panel tween
-			.fromTo("#panel--two", 10, {x: window.innerWidth}, {x: -3000, ease: Power0.easeNone}, "frame2")
+			.fromTo("#panel--two", 10, {x: window.innerWidth, force3D:true}, {x: -3000, force3D:true, ease: Power0.easeNone}, "frame2")
 			.to("#start1", 8, {morphSVG:{shape:"#end1", shapeIndex:16}, ease: Circ.easeOut}, "frame2-=5.5")
 			
 			// add nested panelTimeline
 
-			.fromTo("#panel--three", 10, {x:  window.innerWidth}, {x: -3000, ease: Power0.easeNone})
+			.fromTo("#panel--three", 10, {x:  window.innerWidth, force3D:true}, {x: -3000, force3D:true, ease: Power0.easeNone})
 			
 			// add nested panelTimeline
 
 			
-			.fromTo("#panel--four", 10, {x: window.innerWidth}, {x: -3000, ease: Power0.easeNone})
+			.fromTo("#panel--four", 10, {x: window.innerWidth, force3D:true}, {x: -3000, force3D:true, ease: Power0.easeNone})
 
 			// add nested panelTimeline
 
 
-			.fromTo("#panel--five", 5, {y: window.innerHeight}, {y: '100vh', ease: Power0.easeNone}, "-=2.5")
+			.fromTo("#panel--five", 5, {y: window.innerHeight, force3D:true}, {y: '100vh', force3D:true, ease: Power0.easeNone}, "-=2.5")
 
 			// add nested panelTimeline
 			.add(panel5Timeline)
--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -8,66 +8,35 @@ MorphSVGPlugin.convertToPath("#scroll-icon-middle");
 MorphSVGPlugin.convertToPath("#scroll-icon-bottom");
 
 // findShapeIndex("#start", "#end");
-
 // findShapeIndex("#scroll-icon-top", "#scroll-icon-middle");
 
 $(document).ready(function(){
-
-
-
-	// window.onscroll = function (e) {
-	// 	TweenMax.to(".bottom-nav", .2, {y: 0, ease: Circ.easeOut});
-	// }
-
-
-	// var userHasScrolled = false;
-	
-	// window.onscroll = function (e) {
-	// 	userHasScrolled = true;
-	// }
-
-	// while(userHasScrolled) {
-		
-		
-	// 	TweenMax.to(".bottom-nav", .2, {y: 0, ease: Circ.easeOut});
-
-
-	// 	userHasScrolled = false;
-	// }
-
-
-
-
-
-
 
 	setTimeout(function(){
   		$('.loader-overlay').addClass('loaded');	
 	}, 4000);
 
 	var loadingTimeline = new TimelineMax();
-
 	loadingTimeline
-					.to("#first", .2, {morphSVG:"#second", shapeIndex:16, ease: Circ.easeOut},  "+=1")
-				   	.to("#first", .2, {morphSVG:"#third", shapeIndex:-20, ease: Circ.easeOut}, "+=1")
-				   	.to("#first", .2, {morphSVG:"#first", shapeIndex:-2, ease: Circ.easeOut}, "+=1");
+		.to("#first", .2, {morphSVG:"#second", shapeIndex:16, ease: Circ.easeOut},  "+=1")
+	   	.to("#first", .2, {morphSVG:"#third", shapeIndex:-20, ease: Circ.easeOut}, "+=1")
+	   	.to("#first", .2, {morphSVG:"#first", shapeIndex:-2, ease: Circ.easeOut}, "+=1");
 
-
-	var scrollingTimeline = new TimelineMax({repeat: -1});
-
-	scrollingTimeline
-					.to("#scroll-icon-top", .5, {morphSVG:"#scroll-icon-bottom", shapeIndex:0, ease: Back.easeOut.config(1.7)},  "+=1")
-				   	.to("#scroll-icon-top", .5, {morphSVG:"#scroll-icon-top", shapeIndex:0, ease: Back.easeOut.config(1.7)}, "+=1");			   				   	
+	var scrollingIndicatorTimeline = new TimelineMax({repeat: -1});
+	scrollingIndicatorTimeline
+		.to("#scroll-icon-top", .5, {morphSVG:"#scroll-icon-bottom", shapeIndex:0, ease: Circ.easeOut},  "+=1")
+	   	.to("#scroll-icon-top", .5, {morphSVG:"#scroll-icon-top", shapeIndex:0, ease: Circ.easeOut}, "+=1");			   				   	
 
 
 	$('.logo-2').on('click', function() {
-
 		TweenMax.to("#start-2", 1, {morphSVG:{shape:"#end-2", shapeIndex:28}, ease: Elastic.easeOut.config(1, 0.3)});
 		TweenMax.to("#start-2", 1, {fill: '#fff', ease: Circ.easeOut});
 	});
 	
+	
 	$('#backtop').on('click', function() {
-		$.scrollTo( $('#top'), 1800);
+		console.log('backtop btn clicked!');
+		TweenMax.to(window, 1, {scrollTo:{y:"#top"}});
 	});
 	
 });
